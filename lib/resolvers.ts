1aee52dbb186918bc0c0832bd844cbc75ec8bad0
marcelloantunes <marcello.ants@gmail.com>
2020-01-13 05:20:36 -0300

    add codegem file

--- lib/resolvers.ts
+++
@@ -1,9 +0,0 @@
-const resolvers = {
-  Query: {
-    viewer(_parent, _args, _context, _info) {
-      return { id: 1, name: 'John Smith', status: 'cached' }
-    },
-  },
-}
-
-export default resolvers
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,20 @@
 import withApollo from '../lib/with-apollo'
 import Link from 'next/link'
-import { useViewerQuery } from '../lib/viewer.graphql'
+// import { useViewerQuery } from '../lib/viewer.graphql'
 
 const Index = () => {
-  const { data } = useViewerQuery()
+  // const { data } = useViewerQuery()
 
-  if (data) {
-    const { viewer } = data
+  // if (data) {
+    // const { viewer } = data
     return (
       <div>
-        You're signed in as {viewer.name} and you're {viewer.status} goto{' '}
         <Link href="/about">
           <a>static</a>
         </Link>{' '}
-        page.
       </div>
     )
-  }
+  // }
 
   return <div>...</div>
 }
