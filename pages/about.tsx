964746a6ea0ae7fe768b87c79de53a8c74cf38b1
marcelloantunes <marcello.ants@gmail.com>
2020-01-14 04:33:35 -0300

    fix codegen file

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link'
 
 export default () => (
   <div>
+    This is a static page goto{' '}
     <Link href="/">
       <a>dynamic</a>
     </Link>{' '}
+    page.
   </div>
 )
