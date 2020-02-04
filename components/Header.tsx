46fdaaf8c7bf14ceb60c5b274a397e59e0008103
marcelloantunes <marcello.ants@gmail.com>
2020-02-04 04:49:31 -0300

    create header components/add new query

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  width: 100%
+  height: 100px;
+`
+
+const Header = () => {
+  return (
+    <Wrapper/>
+  )
+}
+
+export default Header
